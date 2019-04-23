cb2a99b739710dfbb0feedae77179d298611999e
renyi <1280401315@qq.com>
2019-04-23 23:47:58 +0800

    Algor_7

--- src/sword/Algor_7.java
+++ src/sword/Algor_7.java
@@ -4,7 +4,7 @@ package sword;
  * 大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项（从0开始，第0项为0）。
  * n<=39
  */
-public class ALgor_7 {
+public class Algor_7 {
     public static void main(String[] args) {
 
     }
