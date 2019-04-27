d63b27c687912b4ebad74c6538b4728d14914928
renyi <1280401315@qq.com>
2019-04-27 22:21:35 +0800

    Algor_—9

--- src/sword/Algor_8.java
+++ src/sword/Algor_8.java
@@ -8,7 +8,7 @@ public class Algor_8 {
     public static void main(String[] args) {
         System.out.println(JumpFloor(3));
     }
-    public static  int JumpFloor(int target) {
+    private static  int JumpFloor(int target) {
         if (target == 1){
             return 1;
         }else if(target == 2){
