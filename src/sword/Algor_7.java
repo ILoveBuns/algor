34553389ac13dc6c2b4a8b310e1eac9dd0b5643d
renyi <1280401315@qq.com>
2019-04-27 21:50:22 +0800

    Algor_7

--- src/sword/Algor_7.java
+++ src/sword/Algor_7.java
@@ -6,11 +6,15 @@ package sword;
  */
 public class Algor_7 {
     public static void main(String[] args) {
-
+        System.out.println(Fibonacci(3));
     }
 
     private static int Fibonacci(int n) {
-
-        return 0;
+        if (n == 0) {
+            return 0;
+        }else if ( n==1 ){
+            return 1;
+        }
+        return Fibonacci(n-1) +Fibonacci(n-2);
     }
 }
