0a4b00708e3f3666df4bc32dc1956057d9767d5c
renyi <1280401315@qq.com>
2019-04-27 22:24:47 +0800

    Algor_10

---
+++ src/sword/Algor_10.java
@@ -0,0 +1,16 @@
+package sword;
+
+/**
+ * 我们可以用2*1的小矩形横着或者竖着去覆盖更大的矩形。
+ * 请问用n个2*1的小矩形无重叠地覆盖一个2*n的大矩形，总共有多少种方法？
+ */
+public class Algor_10 {
+    public static void main(String[] args) {
+
+    }
+
+    private static int RectCover(int target) {
+
+        return 0;
+    }
+}
