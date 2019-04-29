6ba9c8bacb6156a602aa2afd4fcc5b517474acb1
renyi <1280401315@qq.com>
2019-04-29 23:53:20 +0800

    Algor_12

--- src/sword/Algor_11.java
+++ src/sword/Algor_11.java
@@ -5,9 +5,67 @@ package sword;
  */
 public class Algor_11 {
     public static void main(String[] args) {
-
+        System.out.println(NumberOf1(-2147483648));
     }
     private static int NumberOf1(int n) {
+        if (n==0){
+            return 0;
+        }
+        if (n>0){
+            return getNumForPosi(n);
+        }
+        if (n<0){
+            return getNumForNega(n);
+        }
+        return -1;
+    }
 
+    //正数的二进制用辗转相除法
+    private static int getNumForPosi(int n){
+        StringBuffer sb =  getCode(n);
+        return getOne(sb.reverse());
+    }
+
+    //负数的补码为原码，除了最高位外取反
+    private static int getNumForNega(int n){
+        StringBuffer sb =  getCode(-1*n);
+        //除最高位按位取反，得反码
+        sb.setCharAt(0,'1');
+        for (int i = 1; i < sb.length(); i++) {
+            sb.setCharAt(i,sb.charAt(i)=='1'?'0':'1');
+        }
+        System.out.println("反码："+sb.toString());
+        //反码加1
+        for (int i = sb.length()-1; i > 0 ; i--) {
+            if (sb.charAt(i) == '0'){
+                sb.setCharAt(i,'1');
+                break;
+            }
+            sb.setCharAt(i,'0');
+        }
+        System.out.println("补码："+sb.toString());
+        return getOne(sb);
+    }
+
+    //求正数的源码
+    private static StringBuffer getCode(int n){
+        StringBuffer sb =  new StringBuffer();
+        while (sb.length()<32){
+            int shang = n % 2;
+            n = n/2;
+            sb.append(Integer.toString(shang).charAt(0));
+        }
+        return sb.reverse();
+    }
+
+    //计算stringbuffer中1的个数
+    private static int getOne(StringBuffer sb){
+        int k = 0;
+        for (int i = 0; i < sb.length(); i++) {
+            if(sb.charAt(i)=='1'){
+                k++;
+            }
+        }
+        return k;
     }
 }
---
+++ src/sword/Algor_12.java
@@ -0,0 +1,13 @@
+package sword;
+
+/**
+ * 给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
+ */
+public class Algor_12 {
+    public static void main(String[] args) {
+
+    }
+    private double Power(double base, int exponent) {
+
+    }
+}
