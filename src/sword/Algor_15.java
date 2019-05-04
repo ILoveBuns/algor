2e36f6a2743ede32848ca3e909c10393ef948ce1
renyi <1280401315@qq.com>
2019-05-04 23:15:11 +0800

    Algor_15

---
+++ src/sword/Algor_15.java
@@ -0,0 +1,13 @@
+package sword;
+
+/**
+ * 输入一个链表，反转链表后，输出新链表的表头。
+ */
+public class Algor_15 {
+    public static void main(String[] args) {
+
+    }
+    private static ListNode ReverseList(ListNode head) {
+
+    }
+}
