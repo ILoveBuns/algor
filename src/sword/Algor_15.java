02b490955c48a2ada18ad58821cebb658dd63cff
renyi <1280401315@qq.com>
2019-05-05 23:13:49 +0800

    Algor_15

--- src/sword/Algor_15.java
+++ src/sword/Algor_15.java
@@ -5,10 +5,21 @@ package sword;
  */
 public class Algor_15 {
     public static void main(String[] args) {
-
+        ReverseList(init());
     }
     private static ListNode ReverseList(ListNode head) {
-
+        if (head == null) {
+            return null;
+        }
+        ListNode pre = null;
+        while (head.next != null){
+            ListNode tep = head.next;
+            head.next = pre;
+            pre = head;
+            head = tep;
+        }
+        head.next = pre;
+        return head;
     }
 
     private static class ListNode {
