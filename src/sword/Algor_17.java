5ab169690b709584cbb51dfa8a6291e15ca4d7d2
renyi <1280401315@qq.com>
2019-05-06 23:39:56 +0800

    Algor_17

--- src/sword/Algor_17.java
+++ src/sword/Algor_17.java
@@ -8,8 +8,26 @@ public class Algor_17 {
 
     }
 
+    //将两个树先根遍历成字符串序列，然后判断root2序列是否在root1中
     private static boolean HasSubtree(TreeNode root1,TreeNode root2) {
+        if (root2 == null){
+            return false;
+        }
+        StringBuffer sb1 = new StringBuffer();
+        StringBuffer sb2 = new StringBuffer();
+        preTra(root1,sb1);
+        preTra(root2,sb2);
+        return sb1.toString().contains(sb2.toString());
+    }
 
+    //先根遍历
+    private static void preTra(TreeNode root,StringBuffer sb){
+        if (root == null){
+            return;
+        }
+        sb.append(root.val);
+        preTra(root.left,sb);
+        preTra(root.right,sb);
     }
 
     private static class TreeNode {
