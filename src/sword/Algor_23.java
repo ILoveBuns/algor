0c8f317a232574848a78360ea433b53471aba58b
renyi <1280401315@qq.com>
2019-05-08 00:13:07 +0800

    Algor_24

--- src/sword/Algor_23.java
+++ src/sword/Algor_23.java
@@ -1,4 +1,15 @@
 package sword;
 
+/**
+ *
+ 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。
+ 如果是则输出Yes,否则输出No。假设输入的数组的任意两个数字都互不相同。
+ */
 public class Algor_23 {
+    public static void main(String[] args) {
+
+    }
+    public boolean VerifySquenceOfBST(int [] sequence) {
+
+    }
 }
