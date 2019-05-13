0710e8d582aa27c17f8ad55c57f684d65f05b145
jidao <jidao@qunhemail.com>
2019-05-13 18:42:50 +0800

    Algor_24

--- src/sword/Algor_24.java
+++ src/sword/Algor_24.java
@@ -1,8 +1,6 @@
 package sword;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.*;
 
 /**
  *
@@ -11,58 +9,36 @@ import java.util.HashMap;
  (注意: 在返回值的list中，数组长度大的数组靠前)
  */
 
-//通过递归获取所有路径，对符合要求的路径clone后，以路径长度为k、路径为v put进hs中
-//对hs的kset排序后，按照从大到小的顺序从hs中get出路径放到list中，返回list
+//通过递归获取所有路径，对符合要求的路径clone后
 public class Algor_24 {
+
+    private static ArrayList<ArrayList<Integer>> allPath = new ArrayList<>();
+    private static  ArrayList<Integer> path = new ArrayList<>();
+
     public static void main(String[] args) {
-        FindPath(init(),40);
-    }
+        FindPath2(init(),8);
 
-    private static  ArrayList<ArrayList<Integer>> FindPath(TreeNode root,int target){
-        HashMap<Integer,ArrayList<Integer>> hs = new HashMap<>();
-        FindPath1(root,new ArrayList<>(),hs,target);
-
-        //对hs按照key从大到小排序至list中，返回
-        ArrayList<Integer> tep = new ArrayList<>();
-        tep.addAll(hs.keySet());
-        int[] sizeArr = new int[tep.size()];
-        for (int i = 0; i < tep.size(); i++) {
-            sizeArr[i] = tep.get(i);
-        }
-
-        Arrays.sort(sizeArr);
-        ArrayList<ArrayList<Integer>> out = new ArrayList<>();
-        for (int i = sizeArr.length-1; i >= 0 ; i--) {
-            out.add(hs.get(sizeArr[i]));
-            System.out.println(hs.get(sizeArr[i]));
-        }
-        return out;
-    }
-    private static void FindPath1(TreeNode root, ArrayList<Integer> list,HashMap<Integer,ArrayList<Integer>> hs,int target) {
-        if (root == null){
-            return;
-        }
-        list.add(root.val);
-        if(root.left == null && root.right == null){
-            if(equal(list,target)){
-                // 如果list满足要求，将该路径放到map中
-                hs.put(list.size(),(ArrayList<Integer>)list.clone());
+        Collections.sort(allPath, new Comparator<ArrayList<Integer>>() {
+            @Override
+            public int compare(ArrayList<Integer> o1, ArrayList<Integer> o2) {
+                return o2.size() - o1.size();
             }
-            list.remove(list.size()-1);
-            return;
-        }
-        FindPath1(root.left,list,hs,target);
-        FindPath1(root.right,list,hs,target);
-        list.remove(list.size()-1);
+        });
+        System.out.println(allPath);
     }
 
-    private static boolean equal(ArrayList<Integer> list,int target){
-        int sum = 0;
-        for (int i:list) {
-            sum = sum+i;
-        }
-        return sum == target;
+    public static ArrayList<ArrayList<Integer>> FindPath2(TreeNode root,int target) {
+        if(root == null) return allPath;
+        path.add(root.val);
+        target -= root.val;
+        if(target == 0 && root.left == null && root.right == null)
+            allPath.add(new ArrayList<Integer>(path));
+        FindPath2(root.left, target);
+        FindPath2(root.right, target);
+        path.remove(path.size()-1);
+        return allPath;
     }
+
     private static class TreeNode {
         int val = 0;
         TreeNode left = null;
@@ -74,19 +50,15 @@ public class Algor_24 {
     }
 
     private static TreeNode init(){
-        TreeNode Node = new TreeNode(10);
-        Node.left = new TreeNode(5);
-        Node.right = new TreeNode(12);
+        TreeNode Node = new TreeNode(1);
+        Node.left = new TreeNode(2);
+        Node.right = new TreeNode(3);
         Node.left.left = new TreeNode(4);
-        Node.left.right = new TreeNode(7);
-        Node.left.left.left = new TreeNode(3);
-        Node.left.left.left.right = new TreeNode(18);
-        Node.left.right.left = new TreeNode(15);
-        Node.left.right.right = new TreeNode(30);
-        Node.left.right.left.left = new TreeNode(20);
-        Node.left.right.left.right = new TreeNode(21);
-        Node.right.right = new TreeNode(8);
-        Node.right.right.left = new TreeNode(17);
+        Node.left.left.left = new TreeNode(1);
+        Node.left.right = new TreeNode(5);
+        Node.right.right = new TreeNode(3);
+        Node.right.right.left = new TreeNode(1);
+
         return Node;
     }
 }
