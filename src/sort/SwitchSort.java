c6bd3b75d09ca201854dd72b611824c01ef37f92
jidao <1280401315@qq.com>
2022-01-10 00:56:24 +0800

    选择排序

---
+++ src/sort/SwitchSort.java
@@ -0,0 +1,39 @@
+package sort;
+
+public class SwitchSort {
+    public static void main(String[] args) {
+        int[] arr = new int[100000];
+        for (int i = 0; i < arr.length; i++) {
+            int value = (int)(Math.random() * 10000);
+            arr[i] = value;
+        }
+        int[] sortArr = SelectionSort(arr);
+
+        //check
+        for (int a = 0; a < sortArr.length-1; a++) {
+            if (sortArr[a] > sortArr[a+1]){
+                System.out.println("排序错误");
+                return;
+            }
+        }
+        System.out.println("success");
+
+    }
+
+    public static int[] SelectionSort(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            int min = arr[i];
+            int inx = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j] < min){
+                    min = arr[j];
+                    inx = j;
+                }
+            }
+            int tep = arr[i];
+            arr[inx] = tep;
+            arr[i] = min;
+        }
+        return arr;
+    }
+}
--- src/sword/Algor_11.java
+++ src/sword/Algor_11.java
@@ -4,10 +4,10 @@ package sword;
  * 输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。
  */
 public class Algor_11 {
-    public static void main(String[] args) {
-
-    }
-    private static int NumberOf1(int n) {
-
-    }
+//    public static void main(String[] args) {
+//
+//    }
+//    private static int NumberOf1(int n) {
+//
+//    }
 }
