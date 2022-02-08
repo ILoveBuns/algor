b0d85cd15259a295440d1bfa94e8fd022c2d0140
jidao <jidao@qunhemail.com>
2022-02-08 17:53:00 +0800

    1

---
+++ src/graph/BFS.java
@@ -0,0 +1,15 @@
+package graph;
+
+import java.util.HashMap;
+import java.util.Queue;
+
+public class BFS {
+    public static void main(String[] args) {
+        HashMap map = GraphTools.generateGraph();
+    }
+
+    static void BFS(HashMap map){
+
+
+    }
+}
---
+++ src/graph/GraphTools.java
@@ -0,0 +1,24 @@
+package graph;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class GraphTools {
+
+    public static HashMap<String, LinkedList<String>> generateGraph(){
+        HashMap<String, LinkedList<String>> map = new HashMap<>(8);
+
+        map.put("A",new LinkedList<>(){{add("D");add("B");add("C");}});
+        map.put("B",new LinkedList<>(){{add("E");add("F");}});
+        map.put("C",new LinkedList<>(){{add("F");}});
+        map.put("D",new LinkedList<>(){{add("G");add("H");}});
+        map.put("E",new LinkedList<>(){});
+        map.put("F",new LinkedList<>(){});
+        map.put("G",new LinkedList<>(){});
+        map.put("H",new LinkedList<>(){});
+
+        return map;
+    }
+}
