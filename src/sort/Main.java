1498eb0938eaa848d40a13a0359095d3927e4a9f
jidao <1280401315@qq.com>
2022-02-16 00:40:35 +0800

    pub

--- src/sort/Main.java
+++ src/sort/Main.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = ArrTools.generateArrN((int) (Math.random() * 100));
+        int[] arr = ArrTools.generateArrN((int) (Math.random() * 5));
         System.out.println("排序前：" + Arrays.toString(arr));
 
-        insertSort(arr);
+        pubSort(arr);
         System.out.println("排序后：" + Arrays.toString(arr));
         System.out.println("是否递增：" + ArrTools.isIncrement(arr));
     }
@@ -76,6 +76,45 @@ public class Main {
             }
             arr[j+1] = tep;
         }
+    }
 
+    static void shellSort(int arr[]){
+        //dk为分组
+        for (int dk = arr.length/2; dk > 0 ; dk = dk/2) {
+            //firstArrInx代表每个分组的第一个位置
+            for (int firstArrInx = 0; firstArrInx < dk; firstArrInx++) {
+                //在每组内进行简单插入排序
+                for (int j = firstArrInx+dk; j < arr.length ; j = j + dk) {
+                    int tep = arr[j];
+                    //找出每组内第j个位置应当属于的位置
+                    int k;
+                    for (k = j-dk; k >= firstArrInx ; k=k-dk) {
+                        if (tep>arr[k]){
+                            break;
+                        }
+                    }
+                    //从j-dk往前到k+dk开始往后挪一个位置
+                    for (int i = j; i > k+dk ; i=i-dk) {
+                        arr[i] = arr[i-dk];
+                    }
+                    arr[k+dk] = tep;
+                }
+            }
+        }
+    }
+
+    static void pubSort(int arr[]){
+        //共需要遍历N-1次
+        for (int time = 0; time < arr.length-1; time++) {
+            for (int i = 0; i < arr.length-time-1; i++) {
+                if (arr[i]>arr[i+1]){
+                    //交换
+                    int tep = arr[i+1];
+                    arr[i+1]=arr[i];
+                    arr[i]=tep;
+                }
+                System.out.println(Arrays.toString(arr));
+            }
+        }
     }
 }
