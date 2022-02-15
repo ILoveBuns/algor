f35944cfafaaeea633ae367919c5a6aa7749c0f2
jidao <jidao@qunhemail.com>
2022-02-15 15:45:12 +0800

    Algor_5

--- src/sword/Algor_5.java
+++ src/sword/Algor_5.java
@@ -10,19 +10,21 @@ import java.util.Stack;
 
 //1、push,将数据直接压入stack1即可；
 //2、pop,将stack1中的数据弹出压入到stack2中，则数据顺序相反，为保证最先进入的数据一直处于栈顶，
-//只有将stack2中的数据全部pop后，才能继续讲stack1中的数据压入到stack2中，继续做pop
+//   只有将stack2中的数据全部pop后，才能继续讲stack1中的数据压入到stack2中，继续做pop
 public class Algor_5 {
     private static Stack<Integer> stack1 = new Stack<>();
     private static Stack<Integer> stack2 = new Stack<>();
 
     public static void main(String[] args) {
         push(1);
+        System.out.println(pop());
         push(2);
-        System.out.println(pop());
         push(3);
+        System.out.println(pop());
+        push(4);
+        System.out.println(pop());
+        System.out.println(pop());
 
-        System.out.println(pop());
-        System.out.println(pop());
     }
 
     private static void push(int node) {
@@ -30,18 +32,15 @@ public class Algor_5 {
     }
 
     private static int pop() {
-        if(stack1.empty()){
+        if (stack1.isEmpty() && stack2.isEmpty()){
             return -1;
         }
-        int size1 = stack1.size();
-        for (int i = 0; i < size1-1; i++) {
+        if (!stack2.isEmpty()){
+            return stack2.pop();
+        }
+        while (!stack1.isEmpty()){
             stack2.push(stack1.pop());
         }
-        int value = stack1.pop();
-        int size2 = stack2.size();
-        for (int i = 0; i < size2; i++) {
-            stack1.push(stack2.pop());
-        }
-        return value;
+        return stack2.pop();
     }
 }
